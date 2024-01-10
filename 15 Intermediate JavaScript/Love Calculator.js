5bdf0a02a7edcf37f40abd229203a3c0e4335527
mrunmai <mrunmaikudale18@gmail.com>
2024-01-10 22:20:59 +0530

    Love Calculator using Math.random function

---
+++ 15 Intermediate JavaScript/Love Calculator.js
@@ -0,0 +1,7 @@
+prompt("What is your name?");
+prompt("What is your partener's name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.round(loveScore) + 1;
+
+alert("Your love score is "+loveScore+"%");
