715db0c3c728135077fd4aa06bd3b0e15df1cdd9
mrunmai <mrunmaikudale18@gmail.com>
2024-01-20 20:01:07 +0530

    Revert "Generated random no.s to change the dice images"
    This reverts commit cc62299602cb95b04e1df20b8fd3679404a6e210.

--- 17 Dice Game Challenge/Dicee Challenge - Starting Files/index.js
+++
@@ -1,6 +0,0 @@
-var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-document.getElementsByClassName("img1")[0].setAttribute("src","./images/dice"+randomNumber1+".png");
-
-var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-document.getElementsByClassName("img2")[0].setAttribute("src","./images/dice"+randomNumber2+".png");
-
