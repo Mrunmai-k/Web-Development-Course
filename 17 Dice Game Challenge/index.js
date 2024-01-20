75cf0e2e2d5c0b76a15beb04d1b0011737058882
mrunmai <mrunmaikudale18@gmail.com>
2024-01-20 20:22:29 +0530

    Made some changes for h1 text and code formatting

--- 17 Dice Game Challenge/index.js
+++ 17 Dice Game Challenge/index.js
@@ -1,18 +1,17 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-document.getElementsByClassName("img1")[0].setAttribute("src","./images/dice"+randomNumber1+".png");
+var image1 = document.getElementsByClassName("img1")[0];
+image1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
 
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-document.getElementsByClassName("img2")[0].setAttribute("src","./images/dice"+randomNumber2+".png");
+var image2 = document.getElementsByClassName("img2")[0];
+image2.setAttribute("src", "./images/dice" + randomNumber2 + ".png");
 
-if(randomNumber1 > randomNumber2)
-{
-    document.querySelector("h1").innerHTML = "Player 1 Wins";
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins 🚩";
 }
-else if(randomNumber1 < randomNumber2)
-{
-    document.querySelector("h1").innerHTML = "Player 2 Wins";
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
 }
-else
-{
+else {
     document.querySelector("h1").innerHTML = "Draw!";
 }
