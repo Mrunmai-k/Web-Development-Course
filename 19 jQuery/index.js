2dcaf812ae89fd3a7273bf3d530282f1b1922f05
mrunmai <mrunmaikudale18@gmail.com>
2024-01-26 01:05:25 +0530

    Manipulating text with jQuery

--- 19 jQuery/index.js
+++ 19 jQuery/index.js
@@ -1,5 +1,13 @@
+//Manipulating style
 $("h1").addClass("big-title");      //adds class
 
 $("h1").removeClass("big-title");   //removes class
 
 $("h1").hasClass("big-title"); //returns boolean
+
+//Manipulating text
+$("h1").text("Bye");    //changes text
+
+$("button").text("Don't click me");     //changes text for all the buttons
+
+$("button").html("<em>Hey</em>");    //changes innerHTML
