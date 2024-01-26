072064112bc2c6834c0ab61b911e8eb771b39bf5
mrunmai <mrunmaikudale18@gmail.com>
2024-01-27 01:30:54 +0530

    Adding event listeners with jQuery

--- 19 jQuery/index.js
+++ 19 jQuery/index.js
@@ -4,6 +4,7 @@ $("h1").addClass("big-title");      //adds class
 $("h1").removeClass("big-title");   //removes class
 
 $("h1").hasClass("big-title"); //returns boolean
+$("h1").addClass("big-title"); 
 
 //Manipulating text
 $("h1").text("Bye");    //changes text
@@ -17,3 +18,25 @@ $("button").html("<em>Hey</em>");    //changes innerHTML
 console.log($("img").attr("src"));
 
 $("a").attr("href","https://yahoo.com");
+
+//Adding EventListener 
+$("h1").click(function(){
+    $("h1").css("color","purple");
+});
+
+$("button").click(function(){
+    $("h1").css("color","yellow");
+});
+
+$("input").keypress(function(event){
+    console.log(event.key);
+});
+
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+});
+
+$("h1").on("mouseenter",function(){
+    $("h1").css("background-color", "lightgray");
+})
+
