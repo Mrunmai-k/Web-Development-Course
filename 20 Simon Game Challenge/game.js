1bc79d5cd30e757caf279f04b375f4b7ef0cbf02
mrunmai <mrunmaikudale18@gmail.com>
2024-01-28 02:54:28 +0530

    Update h1 based on game has started or not & level

--- 20 Simon Game Challenge/game.js
+++ 20 Simon Game Challenge/game.js
@@ -1,26 +1,49 @@
-var buttonColours = ["red","blue","green","yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
-$("button").on("click",function(event){
+$("button").on("click", function (event) {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
 
     playSound(userChosenColor);
 });
 
-function nextSequence(){
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+function nextSequence() {
+    level++;
+
+    $("#level-title").text("Level " + level);
+    
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
     playSound(randomChosenColour);
+
+
 }
 
-function playSound(name){
-    var audio = new Audio("sounds/"+name+".mp3");
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
