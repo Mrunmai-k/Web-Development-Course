0b3efe963720e7cbb5604927c22b27393be765b3
mrunmai <mrunmaikudale18@gmail.com>
2024-01-28 02:37:46 +0530

    Created a separate function just to play the sound and added event on user chosen button to play the sound

--- 20 Simon Game Challenge/game.js
+++ 20 Simon Game Challenge/game.js
@@ -1,5 +1,13 @@
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+$("button").on("click",function(event){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+});
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
@@ -7,6 +15,12 @@ function nextSequence(){
     gamePattern.push(randomChosenColour);
 
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
 }
 
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
 
