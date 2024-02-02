697b801e98b8e657a35b4e9e3cb8785517ed978b
mrunmai <mrunmaikudale18@gmail.com>
2024-02-02 19:23:59 +0530

    npm package installation and changing cjs to ecm

---
+++ 23 Node.js/2.3 NPM/index.js
@@ -0,0 +1,6 @@
+// var generateName = require('sillyname'); //commonjs
+
+import generateStupidName from 'sillyname';
+var sillyName = generateStupidName();
+
+console.log(`My name is ${sillyName}.`);
