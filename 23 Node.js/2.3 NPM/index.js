90b4c8cb134e64840d92742b5448499d238f0234
mrunmai <mrunmaikudale18@gmail.com>
2024-02-02 19:35:40 +0530

    Installed another pkg to generate superheroes names

--- 23 Node.js/2.3 NPM/index.js
+++ 23 Node.js/2.3 NPM/index.js
@@ -1,6 +1,14 @@
 // var generateName = require('sillyname'); //commonjs
 
-import generateStupidName from 'sillyname';
-var sillyName = generateStupidName();
+// import generateStupidName from 'sillyname';
+// var sillyName = generateStupidName();
 
-console.log(`My name is ${sillyName}.`);
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from 'superheroes';
+ 
+const name = superheroes.random();
+
+console.log(`I am ${name}`);
+ 
+
