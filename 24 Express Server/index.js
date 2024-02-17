817b62070601f16a696e427f111b8a319f397323
mrunmai <mrunmaikudale18@gmail.com>
2024-02-18 01:02:26 +0530

    GET requests for about and contact endpoints

--- 24 Express Server/index.js
+++ 24 Express Server/index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
     res.send("<h1>Hello World</h1>");
 })
 
+app.get("/contact",(req,res) => {
+    res.send("Contact: +12 123456789<br>Mail:abc@yahoo.com");
+})
+
+app.get("/about",(req,res) => {
+    res.send("This is a practice Express server to understand HTTP requests");
+})
+
 app.listen(port, () => {
     console.log(`The server is running on port ${port}.`);
 })
