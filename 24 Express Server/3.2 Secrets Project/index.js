b7c705b8a5ddc0c6e5bde22bce1abffb548922e7
mrunmai <mrunmaikudale18@gmail.com>
2024-02-20 12:35:55 +0530

    Basic express server for secret access project

---
+++ 24 Express Server/3.2 Secrets Project/index.js
@@ -0,0 +1,13 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+})
+
+
