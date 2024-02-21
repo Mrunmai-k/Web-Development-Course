5cbd28bad3d996ec8dd8e2ca046384ad39b21e5c
mrunmai <mrunmaikudale18@gmail.com>
2024-02-21 12:40:58 +0530

    Handled the get request using path, url packages

--- 24 Express Server/3.2 Secrets Project/index.js
+++ 24 Express Server/3.2 Secrets Project/index.js
@@ -3,8 +3,16 @@
 //The password is ILoveProgramming
 import express from "express";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
+
+app.get("/check", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
