ba14a14748fd5d7166ffa854d6dcb2b8857cb8af
mrunmai <mrunmaikudale18@gmail.com>
2024-02-21 13:00:03 +0530

    Imported body-parser and used as a middleware

--- 24 Express Server/3.2 Secrets Project/index.js
+++ 24 Express Server/3.2 Secrets Project/index.js
@@ -1,7 +1,16 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+// My Steps:
+// 1. Create baxic express server
+// 2. handle the get request and show index.html
+// 3. import the body-parser and use it as middleware
+// 4. Create a function for checking password(pre-processing) and use it as a middleware
+// 5. Handle the post request 
+
 import express from "express";
+import bodyParser from "body-parser";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -10,9 +19,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.get("/check", (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
-})
+});
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
