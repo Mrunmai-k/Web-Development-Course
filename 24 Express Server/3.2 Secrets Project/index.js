29bbac8516a2199eb37b95acc1769faac8fee867
mrunmai <mrunmaikudale18@gmail.com>
2024-02-21 13:01:26 +0530

    Create a custom middleware to check the password is correct or not

--- 24 Express Server/3.2 Secrets Project/index.js
+++ 24 Express Server/3.2 Secrets Project/index.js
@@ -18,8 +18,20 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var password = "";
+var isUserAuth = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+function checkPassword(req,res,next){
+    password = req.body['password'];
+    if(password === "ILoveProgramming"){
+        isUserAuth = true;
+    }
+    next();
+}
+
+app.use(checkPassword);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
