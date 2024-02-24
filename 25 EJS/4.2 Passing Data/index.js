993ca0f16c1a82ad08af99f7f16b521751607872
mrunmai <mrunmaikudale18@gmail.com>
2024-02-24 19:16:59 +0530

    Calculate length of words passed through html form

--- 25 EJS/4.2 Passing Data/index.js
+++ 25 EJS/4.2 Passing Data/index.js
@@ -6,9 +6,15 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const len = req.body["fName"].length + req.body["lName"].length;
+  // console.log(len);
+  res.render("index.ejs",{numOfLetters:len});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
