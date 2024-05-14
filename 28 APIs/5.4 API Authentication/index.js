b12069e9c3d55fce63e15a065f5609d4f7c5fea3
mrunmai <mrunmaikudale18@gmail.com>
2024-05-15 01:21:32 +0530

    Changed the credentials

--- 28 APIs/5.4 API Authentication/index.js
+++ 28 APIs/5.4 API Authentication/index.js
@@ -8,13 +8,8 @@ const API_URL = "https://secrets-api.appbrewery.com";
 //TODO 1: Fill in your values for the 3 types of auth.
 const yourUsername = "AnneHathway";
 const yourPassword = "TheIdeaOfYou";
-<<<<<<< HEAD
 const yourAPIKey = "0f20d188-0a43-456c-9b79-5fd369e1277f";
 const yourBearerToken = "d27484a9-fd7b-46b5-a5e9-61a64a173574";
-=======
-const yourAPIKey = "91945f65-f51b-4336-a41e-30bdf92061a4";
-const yourBearerToken = "8de4989e-320a-4642-8471-29d25b81f440";
->>>>>>> 6eb411ff47d8249eb907f4165455c47a6e77c7ac
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
@@ -26,15 +21,9 @@ app.get("/noAuth", async (req, res) => {
   //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
   try {
     const result = await axios.get(API_URL + "/random");
-<<<<<<< HEAD
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
     res.status(404).send(error.message);
-=======
-    res.render("index.ejs",{content: JSON.stringify(result.data)});
-  } catch (error) {
-    res.status(404).send("Error:" + error.message);
->>>>>>> 6eb411ff47d8249eb907f4165455c47a6e77c7ac
   }
 });
 
--- 28 APIs/5.4 API Authentication/solution.js
+++ 28 APIs/5.4 API Authentication/solution.js
@@ -8,13 +8,8 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // TODO: Replace the values below with your own before running this file.
 const yourUsername = "AnneHathway";
 const yourPassword = "TheIdeaOfYou";
-<<<<<<< HEAD
 const yourAPIKey = "0f20d188-0a43-456c-9b79-5fd369e1277f";
 const yourBearerToken = "d27484a9-fd7b-46b5-a5e9-61a64a173574";
-=======
-const yourAPIKey = "91945f65-f51b-4336-a41e-30bdf92061a4";
-const yourBearerToken = "8de4989e-320a-4642-8471-29d25b81f440";
->>>>>>> 6eb411ff47d8249eb907f4165455c47a6e77c7ac
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
