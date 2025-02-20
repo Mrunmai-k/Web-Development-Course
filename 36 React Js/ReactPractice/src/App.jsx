84a159333a5ca43af1ebfa0cbcb352b4af6fccd4
mrunmai <mrunmaikudale18@gmail.com>
2025-02-20 20:28:56 +0530

    Import and Export Practice Challenge

--- 36 React Js/ReactPractice/src/App.jsx
+++ 36 React Js/ReactPractice/src/App.jsx
@@ -1,10 +1,17 @@
 import StyleDemo from "./StyleDemo"
+import { add, subtract, multiply, divide } from "./Calculator";
 
 function App() {
 
   return (
     <>
       <StyleDemo/>
+      <ul>
+        <li>{add(10,20)}</li>
+        <li>{subtract(100,20)}</li>
+        <li>{multiply(10,20)}</li>
+        <li>{divide(10,20)}</li>
+      </ul>
     </>
   )
 }
---
+++ 36 React Js/ReactPractice/src/Calculator.jsx
@@ -0,0 +1,14 @@
+function add(no1, no2) {
+    return no1 + no2;
+  }
+  function subtract(no1, no2) {
+    return no1 - no2;
+  }
+  function multiply(no1, no2) {
+    return no1 * no2;
+  }
+  function divide(no1, no2) {
+    return no1 / no2;
+  }
+  export { add, subtract, multiply, divide };
+  
