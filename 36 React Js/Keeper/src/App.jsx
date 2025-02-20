9920791ef7bbf8314ac31fb5ee6a9ceaa910677d
mrunmai <mrunmaikudale18@gmail.com>
2025-02-20 22:35:05 +0530

    Keeper App Task

---
+++ 36 React Js/Keeper/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import Note from "./Note"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Note/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ 36 React Js/Keeper/src/Note.jsx
@@ -0,0 +1,19 @@
+function Note() {
+  return (
+    <>
+      <div className="note">
+        <h1>Buy Milk</h1>
+        <p>Buy milk from grocery shop</p>
+      </div>
+      <div className="note">
+        <h1>Buy Milk</h1>
+        <p>Buy milk from grocery shop</p>
+      </div>
+      <div className="note">
+        <h1>Buy Milk</h1>
+        <p>Buy milk from grocery shop</p>
+      </div>
+    </>
+  );
+}
+export default Note;
